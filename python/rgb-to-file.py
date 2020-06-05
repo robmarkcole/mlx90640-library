f6b82dd31e1f2c11ca1f83d4dbffbb74d4691489
Robin Cole <robmarkcole@gmail.com>
2020-06-05 08:34:48 +0100

    update

--- python/rgb-to-file.py
+++ python/rgb-to-file.py
@@ -1,8 +1,8 @@
 """
-Robin edit of rgb-to-gif.py to save a single frame to jpg.
+Save a single frame to jpg.
 """
 
-#!/usr/bin/env python3
+import argparse
 import time
 from datetime import datetime
 import subprocess
@@ -11,21 +11,41 @@ from PIL import Image
 import os
 
 RAW_RGB_PATH = "../examples/rawrgb"
+DEFAULT_FILENAME = "mlx90640-{}.jpg".format(
+    datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+)
 
-if not os.path.isfile(RAW_RGB_PATH):
-    raise RuntimeError("{} doesn't exist, did you forget to run \"make\"?".format(RAW_RGB_PATH))
 
-with subprocess.Popen(["sudo", RAW_RGB_PATH], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as camera:
-	# Despite the docs, we use read() here since we want to poll
+def main():
+    parser = argparse.ArgumentParser(description="Save a single capture")
+    parser.add_argument(
+        "--filename",
+        default=DEFAULT_FILENAME,
+        type=str,
+        help="Name for file, including extension .jpg",
+    )
+    args = parser.parse_args()
+
+    if not os.path.isfile(RAW_RGB_PATH):
+        raise RuntimeError(
+            '{} doesn\'t exist, did you forget to run "make"?'.format(RAW_RGB_PATH)
+        )
+
+    with subprocess.Popen(
+        ["sudo", RAW_RGB_PATH],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    ) as camera:
+        # Despite the docs, we use read() here since we want to poll
         # the process for chunks of 2304 bytes, each of which is a frame
         frame = camera.stdout.read(2304)
 
         # Convert the raw frame bytes into a PIL image and resize
-        image = Image.frombytes('RGB', (24, 32), frame)
+        image = Image.frombytes("RGB", (24, 32), frame)
+        print("Saving {} ".format(args.filename))
+        image.save(args.filename)
 
-        filename = 'mlx90640-{}.jpg'.format(
-            datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
 
-        print("Saving {} ".format(filename))
-        image.save(filename,)
-
+if __name__ == "__main__":
+    main()
