00292b2d7437cad0067389e0326ecf248e5b4ab3
Robin Cole <robmarkcole@gmail.com>
2020-06-05 09:41:05 +0100

    Update rgb-to-file.py

--- python/rgb-to-file.py
+++ python/rgb-to-file.py
@@ -9,11 +9,14 @@ import subprocess
 import sys
 from PIL import Image
 import os
+import numpy as np
+from matplotlib import cm
 
 RAW_RGB_PATH = "../examples/rawrgb"
 DEFAULT_FILENAME = "mlx90640-{}.jpg".format(
     datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 )
+DEFAULT_FRAMEDROP = 5
 
 
 def main():
@@ -23,6 +26,12 @@ def main():
         default=DEFAULT_FILENAME,
         type=str,
         help="Name for file, including extension .jpg",
+    )
+    parser.add_argument(
+        "--dropframes",
+        default=DEFAULT_FRAMEDROP,
+        type=int,
+        help="Number of frames to drop, default {}".format(DEFAULT_FRAMEDROP),
     )
     args = parser.parse_args()
 
@@ -39,10 +48,15 @@ def main():
     ) as camera:
         # Despite the docs, we use read() here since we want to poll
         # the process for chunks of 2304 bytes, each of which is a frame
-        frame = camera.stdout.read(2304)
+        # Capture two frames and average to remove checkerboard
 
-        # Convert the raw frame bytes into a PIL image and resize
-        image = Image.frombytes("RGB", (24, 32), frame)
+        frames_to_drop = args.dropframes  # since the camera needs to warm up
+        for i in range(frames_to_drop):
+            camera.stdout.read(2304)
+            print("Capture in {}".format(frames_to_drop - i))
+
+        frame = camera.stdout.read(2304)
+        image = Image.frombytes("RGB", (32, 24), frame)
         print("Saving {} ".format(args.filename))
         image.save(args.filename)
 
